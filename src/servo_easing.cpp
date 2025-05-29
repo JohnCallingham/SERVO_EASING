9b44967bb373093fa28fa5fe344be54a553910ff
JohnCallingham <JohnCallingham@users.noreply.github.com>
2025-05-29 22:19:20 +0100

    Updated code

---
+++ src/servo_easing.cpp
@@ -0,0 +1,26 @@
+#include "servo_easing.h"
+
+bool SERVO_EASING::update() {
+  // Implement a non blocking delay for delaymS.
+  if (millis() < nextUpdate) return false;
+  nextUpdate = millis() + delaymS;
+
+  // The delay has expired so it's time to check if any movement is required.
+  if (currentAngle == targetAngle) return false;
+
+  if (currentAngle < targetAngle) {
+    currentAngle++; // currentAngle needs to increase.
+  }
+
+  if (currentAngle > targetAngle) {
+    currentAngle--; // currentAngle needs to decrease.
+  }
+
+  // Has the currentAngle reached the targetAngle yet?
+  if (currentAngle == targetAngle) return true;
+
+  return false;
+}
+
+
+// HOW to pass back the current servo angle so that the servo can move???
---
+++ src/servo_easing.h
@@ -0,0 +1,32 @@
+#ifndef SERVO_EASING_H
+#define SERVO_EASING_H
+
+#include <Arduino.h>
+
+/**
+ * Contains the servo current angle.
+ * Handles the movement of the servo angle towards the target angle.
+ */
+
+class SERVO_EASING {
+  public:
+    void initialise(uint8_t initialAngle) { this->currentAngle = initialAngle; }
+    void setTargetAngle(uint8_t targetAngle) { this->targetAngle = targetAngle; }
+    void setDelaymS(unsigned long delaymS) { this->delaymS = delaymS; }
+
+    /**
+     * Checks to see if the currentAngle needs to be changed.
+     * Returns false if there is no need to move the servo, or it is still moving.
+     * Returns true if the currentAngle has reached the targetAngle.
+     */
+    bool update();
+
+  private:
+    uint8_t currentAngle;
+    uint8_t targetAngle;
+    unsigned long delaymS = 100; // Default is 100 mS.
+    unsigned long nextUpdate = 0;
+
+};
+
+#endif

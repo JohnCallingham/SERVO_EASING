d7ace805a6be9e011bc5ee62e443f19eb3b08956
JohnCallingham <JohnCallingham@users.noreply.github.com>
2025-06-29 10:36:55 +0100

    Updated code

--- src/servo_easing.cpp
+++ src/servo_easing.cpp
@@ -5,21 +5,33 @@ void ServoEasing::update() {
   if (millis() < nextUpdate) return;
   nextUpdate = millis() + delaymS;
 
+  uint8_t differenceAngle;
+
   // The delay has expired so it's time to check if any movement is required.
   if (currentAngle == targetAngle) return;
 
   if (currentAngle < targetAngle) {
+    differenceAngle = targetAngle - currentAngle;
     currentAngle++; // currentAngle needs to increase.
     direction = AngleDirection::INCREASING_ANGLE;
   }
 
   if (currentAngle > targetAngle) {
+    differenceAngle = currentAngle - targetAngle;
     currentAngle--; // currentAngle needs to decrease.
     direction = AngleDirection::DECREASING_ANGLE;
   }
 
   this->servo->write(currentAngle);
-  Serial.printf("\nservo %d current angle = %d", servoNumber, currentAngle);
+  Serial.printf("\n%6ld servo %d current angle = %d", millis(), servoNumber, currentAngle);
+
+  // If the current angle is nearing the target angle then increase the delay between updates.
+  if (differenceAngle < 10) {
+    nextUpdate += delaymS;
+  }
+  if (differenceAngle < 5) {
+    nextUpdate += delaymS;
+  }
 
   // Has the current angle reached the mid angle yet?
   if (currentAngle == midAngle) {

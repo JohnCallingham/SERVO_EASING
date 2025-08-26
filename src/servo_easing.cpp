785a54845d7a2029bb74155229db1a670d398e31
JohnCallingham <JohnCallingham@users.noreply.github.com>
2025-08-26 13:41:20 +0100

    Updated code

--- src/servo_easing.cpp
+++ src/servo_easing.cpp
@@ -7,30 +7,31 @@ void ServoEasing::initialise(uint8_t servoNumber, uint8_t servoPin) {
    * Initialise the PWM pulses.
    * Uses servoNumber for the PWM channel.
    */
+  Serial.printf("\n%6ld setting up PWM for servo %d", millis(), servoNumber);
+
   ledcSetup(this->servoNumber, PWM_FREQ, PWM_RESOLUTION);
   ledcAttachPin(servoPin, this->servoNumber);
 
   // Start with no PWM pulses.
   ledcWrite(this->servoNumber, 0);
+
+  // Need a delay to ensure that the PWM duty cycle is 0.
+  delay(100); // 50 mS doesn't work.
 }
 
 void ServoEasing::setInitialAngle(uint8_t initialAngle) {
   this->currentAngle = initialAngle;
-  
+
   // Move the servo to its initial angle. There is no easing.
   updatePWM(initialAngle);
 
-  // Allow some time for servo to move. How much??
-  delay(1000);
-
-  // Stop the PWM pulses.
-  ledcWrite(this->servoNumber, 0);
+  // The update() function will remove PWM pulses.
 }
 
 void ServoEasing::setTargetAngle(uint8_t targetAngle) {
   this->targetAngle = targetAngle;
 
-  // Update the start and finish angles and ticks for this movement.
+  // Update the start and finish angles for this movement.
   movementStartAngle = currentAngle;
   movementFinishAngle = targetAngle;
 }
@@ -45,16 +46,22 @@ void ServoEasing::update() {
   /**
    * The delay has expired so it's time to check if any movement is required.
    */
-  if (currentAngle == targetAngle) return;
+  if (this->currentAngle == this->targetAngle) {
+    // PWM pulses are turned off here to allow time for the servo to complete its final step.
+    if (ledcRead(this->servoNumber > 0)) {
+      // Stop any more PWM pulses.
+      Serial.printf("\n%6ld servo %d turning off PWM pulses", millis(), servoNumber);
+      ledcWrite(this->servoNumber, 0);
+    }
 
-  /**
-   * Movement is required so check which direction.
-   */
+    return;
+  }
+
+  // Movement is required so check which direction.
   if (currentAngle < targetAngle) {
     currentAngle++; // currentAngle needs to increase.
     direction = AngleDirection::INCREASING_ANGLE;
   }
-
   if (currentAngle > targetAngle) {
     currentAngle--; // currentAngle needs to decrease.
     direction = AngleDirection::DECREASING_ANGLE;
@@ -64,16 +71,20 @@ void ServoEasing::update() {
   updatePWM(currentAngle);
   Serial.printf("\n%6ld servo %d current angle = %d", millis(), servoNumber, currentAngle);
 
-  /**
-   * A crude form of servo easing.
-   * Calculates the current percentage of the movement.
-   * Varies the delay at the start and end of the movement.
-   */
-  uint16_t perCent = map(currentAngle, movementStartAngle, movementFinishAngle, 1, 100 );
-  if (perCent < 10) nextUpdate += (3 * delaymS);
-  if (perCent < 20) nextUpdate += (2 * delaymS);
-  if (perCent > 80) nextUpdate += (2 * delaymS);
-  if (perCent > 90) nextUpdate += (3 * delaymS);
+  if (easingAction != EasingAction::EASE_NONE) {
+    // TO DO: need to add code for EASE_IN or EASE_OUT only.
+
+    /**
+     * A crude form of servo easing.
+     * Calculates the current percentage of the movement.
+     * Varies the delay at the start and end of the movement.
+     */
+    uint16_t perCent = map(currentAngle, movementStartAngle, movementFinishAngle, 1, 100 );
+    if (perCent < 10) nextUpdate += (3 * delaymS);
+    if (perCent < 20) nextUpdate += (2 * delaymS);
+    if (perCent > 80) nextUpdate += (2 * delaymS);
+    if (perCent > 90) nextUpdate += (3 * delaymS);
+  }
 
   // Has the current angle reached the mid angle yet?
   if (currentAngle == midAngle) {
@@ -83,8 +94,8 @@ void ServoEasing::update() {
 
   // Has the current angle reached the target angle yet?
   if (currentAngle == targetAngle) {
-    // Stop any more PWM pulses.
-    ledcWrite(this->servoNumber, 0);
+    // Do not turn PWM pulses off here so that there is time for the servo to move before its PWM pulses are removed.
+    // Moved to the top of this function.
 
     // Call the callback function if one has been set.
     if (reachedAngle) reachedAngle(servoNumber, currentAngle, direction);
@@ -95,9 +106,18 @@ void ServoEasing::update() {
 
 void ServoEasing::updatePWM(uint8_t servoAngle) {
   // Convert servoAngle to duty cycle (ticks).
-  uint16_t dutyCycle = map(servoAngle, MIN_ANGLE, MAX_ANGLE, MIN_TICKS, MAX_TICKS);
-  Serial.printf("\ndutyCycle = %d", dutyCycle);
+  uint32_t dutyCycle = map(servoAngle, MIN_ANGLE, MAX_ANGLE, MIN_TICKS, MAX_TICKS);
 
   // Write the new duty cycle to the PWM controller.
   ledcWrite(this->servoNumber, dutyCycle);
+}
+
+void ServoEasing::easeTo(uint8_t targetAngle) {
+  easingAction = EasingAction::EASE_IN_OUT;
+  setTargetAngle(targetAngle);
+}
+
+void ServoEasing::moveTo(uint8_t targetAngle) {
+  easingAction = EasingAction::EASE_NONE;
+  setTargetAngle(targetAngle);
 }
--- src/servo_easing.h
+++ src/servo_easing.h
@@ -1,27 +1,16 @@
 #ifndef SERVO_EASING_H
 #define SERVO_EASING_H
 
-/**
- * FEETECH (formerly known as Fitec) FS90 servo.
- * Specifications;-
- * https://www.addicore.com/products/feetech-fitec-fs90-9g-mini-servo-with-accessories
- * 
- * Description;-
- * The FS90 is a 9g analog servo from FEETECH (formerly known as Fitec)
- * which is similar in form and function to the Towerpro SG90.
- * The specifications state that this servo has a 120° operating angle
- * for standard servo pulses between 900 µs and 2100 µs.
- * As with most servos, the pulse range can be expanded to achieve
- * an expanded operating angle,
- * but the limits of which are not specified by FEETECH.
- */
-
- #include <Arduino.h>
+#include <Arduino.h>
 
 #define PWM_FREQ               50 // 50 Hz.
-#define MICRO_SECONDS_PER_CYCLE 20000 // 20,000 microSeconds in a cycle of frequency 50 Hz.
-// #define PWM_RESOLUTION         13  // Needs a minimum of 10 bit resolution, 8 bit doesn't work down to 50Hz !!
-#define PWM_RESOLUTION         14  // Needs a minimum of 10 bit resolution, 8 bit doesn't work down to 50Hz !!
+#define MICRO_SECONDS_PER_CYCLE 20000 // 20,000 microSeconds in one cycle of frequency 50 Hz.
+
+// #define PWM_RESOLUTION         13  // Needs a minimum of 10 bit resolution, 8 bit doesn't work down to 50 Hz !!
+#define PWM_RESOLUTION         14  // Needs a minimum of 10 bit resolution, 8 bit doesn't work down to 50 Hz !!
+
+// #define TICKS_PER_CYCLE      8192 // For a PWM_RESOLUTION of 13 bits.
+#define TICKS_PER_CYCLE         16384 // For a PWM_RESOLUTION of 14 bits.
 
 #define MIN_ANGLE               0
 #define MAX_ANGLE             180
@@ -31,13 +20,8 @@
 #define MAX_PULSE_WIDTH      2400 // The number of microSeconds for MAX_ANGLE.
 #define DEFAULT_PULSE_WIDTH  1500 // The number of microSeconds for DEFAULT_ANGLE.
 
-// #define TICKS_PER_CYCLE      8192 // For a PWM_RESOLUTION of 13 bits.
-#define TICKS_PER_CYCLE      (32768/2) // For a PWM_RESOLUTION of 14 bits.
-
 const long MIN_TICKS = (MIN_PULSE_WIDTH * TICKS_PER_CYCLE) / MICRO_SECONDS_PER_CYCLE;
 const long MAX_TICKS = (MAX_PULSE_WIDTH * TICKS_PER_CYCLE) / MICRO_SECONDS_PER_CYCLE;
-
-enum AngleDirection { INCREASING_ANGLE, DECREASING_ANGLE };
 
 /**
  * Contains the servo current angle.
@@ -45,9 +29,10 @@ enum AngleDirection { INCREASING_ANGLE, DECREASING_ANGLE };
  */
 class ServoEasing {
   public:
+    enum AngleDirection { INCREASING_ANGLE, DECREASING_ANGLE };
+
     void initialise(uint8_t servoNumber, uint8_t servoPin);
     void setInitialAngle(uint8_t initialAngle);
-    void setTargetAngle(uint8_t targetAngle);
     void setMidAngle(uint8_t midAngle) { this->midAngle = midAngle; }
 
     void setReachedAngleCallbackFunction(void (*reachedAngle)(uint8_t servoNumber, uint8_t currentAngle, AngleDirection direction)) { this->reachedAngle = reachedAngle; }
@@ -55,25 +40,41 @@ class ServoEasing {
     uint8_t getCurrentAngle() { return this->currentAngle; }
 
     /**
+     * Causes the servo to move from its current angle to the target angle with easing in and out.
+     */
+    void easeTo(uint8_t targetAngle);
+
+    /**
+     * Causes the servo to move to the target angle with no easing in or out.
+     */
+    void moveTo(uint8_t targetAngle);
+
+    /**
      * delaymS is the delay between each change to the current angle.
      */
     void setDelaymS(unsigned long delaymS) { this->delaymS = delaymS; }
 
     /**
-     * Checks to see if the currentAngle needs to be changed.
+     * Called repeadedly and checks to see if the current angle needs to be changed.
      */
     void update();
 
   private:
+    enum EasingAction { EASE_NONE, EASE_IN, EASE_OUT, EASE_IN_OUT };
+    EasingAction easingAction = EASE_IN_OUT;
+    
     AngleDirection direction;
+
     uint8_t servoNumber;
 
     uint8_t currentAngle;
     uint8_t targetAngle;
-    uint8_t midAngle = 90; // Defaults to 90 degrees.
+    uint8_t midAngle = 90; // Defaults to 90 degrees, but user configurable.
 
-    unsigned long delaymS; // Default is 100 mS. no default as now user configuratble.
+    unsigned long delaymS; // User configuratble.
     unsigned long nextUpdate = 0;
+
+    void setTargetAngle(uint8_t targetAngle);
 
     // A callback function for when the current angle reaches the mid or target angles.
     void (*reachedAngle)(uint8_t servoNumber, uint8_t currentAngle, AngleDirection direction);
@@ -81,12 +82,9 @@ class ServoEasing {
     // Calculates the duty cycle for the current angle and updates the PWM.
     void updatePWM(uint8_t currentAngle);
 
-    // // Stores the start and finish angles and ticks for the current movement.
     // Stores the start and finish angles for the current movement.
     uint8_t movementStartAngle;
     uint8_t movementFinishAngle;
-    // uint16_t movementStartTick;
-    // uint16_t movementFinishTick;
 };
 
 #endif
